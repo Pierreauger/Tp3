2390b9e1210db0b8e3e500fddb8de17120ec78a9
Pierreauger <pierre.auger@isen-ouest.yncrea.fr>
2020-03-20 10:01:23 +0100

    jk

--- string.js
+++ string.js
@@ -1,3 +1,10 @@
 function repeat(s,n){
    return s.repeat(n);
 }
+function truncate(s,n){
+    return s.substr(0, n-3);
+}
+/*function ispalindrome(s){
+for(let i = 0; i<(s.length)/2; i++){
+if 
+}*/
--- stringTest.js
+++ stringTest.js
@@ -1,3 +1,6 @@
 function repeatTest(){
     console.log(repeat("foo", 3)); // => foofoofoo
+    console.log(truncate("unchanged text", 20)); // => unchanged text
+    console.log(truncate("unchanged text", 14)); // => unchanged text
+    console.log(truncate("truncated text", 10)); // => truncat...
 }
