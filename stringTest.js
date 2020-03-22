4654d51e4c7cb96724567d6a9fe28bbd03f47e91
Pierreauger <pierre.auger@isen-ouest.yncrea.fr>
2020-03-22 11:37:20 +0100

    df

--- stringTest.js
+++ stringTest.js
@@ -3,4 +3,8 @@ function repeatTest(){
     console.log(truncate("unchanged text", 20)); // => unchanged text
     console.log(truncate("unchanged text", 14)); // => unchanged text
     console.log(truncate("truncated text", 10)); // => truncat...
+    console.log(isPalindrome("bonjour")); // => false
+    console.log(isPalindrome("toto")); // => false
+    console.log(isPalindrome("kayak")); // => true
+    console.log(isPalindrome("ressasser")); // => true
 }
